108a5112a55e88ba978884ff6f3c6742ed8ba23d
Taylor <ttaylor-st@murena.io>
2024-06-24 21:01:22 +0100

    expand readme, add header comment to hello_world.sv

--- src/hello_world.sv
+++ src/hello_world.sv
@@ -1,8 +1,14 @@
+// Verilog module to print "Hello World!" to the console
+//
+// Running:
+// $ verilator --binary -j 0 --cc hello_world.sv
+// $ ./obj_dir/Vhello_world
+
 module hello_world ();
 
-  initial begin
-    $display("Hello World!");
-    $finish;
-  end
+    initial begin
+        $display("Hello World!");
+        $finish;
+    end
 
 endmodule
